e9989724aba592abf164e76383abdeacaaf5c009
John Thantranon <john@uniconstudios.com>
2016-10-18 13:57:21 -0700

    Updated at hyperdev.com

--- server.js
+++ server.js
@@ -1,65 +1,63 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
+var Chance = require('chance');
+var chance = new Chance();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// Simple in-memory store for now
-var dreams = [];
+var dreams = [
+  'placeholder',
+  '================================================='
+];
 var generation = 1;
 
 app.get("/dreams", function (request, response) {
   generation = 1;
-  dreams = [];
-  go(1,1000);
+  dreams = [
+    'placeholder',
+    '================================================='
+  ];
+  go(1,5000);
   response.send(dreams);
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
 app.post("/dreams", function (request, response) {
   dreams.push(request.query.dream);
   response.sendStatus(200);
 });
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-var Chance = require('chance');
-var chance = new Chance();
-
-
-
 function die(){
-    console.log('--- die ---');
+    var msg = '--- (a) an alien dies [-1] ---';
+    console.log(msg);
+    dreams.push(msg);
     return 0;
 }
 
 function nothing(){
-    console.log('--- nothing ---');
+    var msg = '--- (b) an alien does nothing [+0] ---';
+    console.log(msg);
+    dreams.push(msg);
     return 1;
 }
 
 function replicate(){
-    console.log('--- replicate ---');
+    var msg = '--- (c) an alien replicates itself (2) [+1] ---';
+    console.log(msg);
+    dreams.push(msg);
     return 2;
 }
 
 function multiply(){
-    console.log('--- multiply ---');
+    var msg = '--- (d) an alien replicates itself twice (3) [+2] ---';
+    console.log(msg);
+    dreams.push(msg);
     return 3;
 }
 
@@ -84,11 +82,13 @@ function go(startingPopulation,limit){
     if(newPop > limit) {
       var msg = `Alien Population Limit of [${limit}] reached at Generation [${generation}] with a population of [${newPop}]`
       console.log(msg);
+      dreams[0] = msg;
       dreams.push(msg);
       // dreams[0] = newPop;
     }
     if(newPop === 0) {
       var msg = `Alien Population Extinct at Generation [${generation}]`
+      dreams[0] = msg;
       console.log(msg);
       dreams.push(msg);
     }
